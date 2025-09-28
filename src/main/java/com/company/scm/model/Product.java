f35c2e347fef05a3e30728cb0b169addfc42d192
Harika Kada <harikakada1024@outlook.com>
2025-09-28 21:54:10 +0530

    Updated Product.java

--- src/main/java/com/company/scm/model/Product.java
+++ src/main/java/com/company/scm/model/Product.java
@@ -1,5 +1,35 @@
 package com.company.scm.model;
 
-public class Product {
+import java.math.BigDecimal;
 
+public class Product {
+    private final String productId;
+    private final String name;
+    private final String description;
+    private final String category;
+    private final BigDecimal unitPrice;
+
+    public Product(String productId, String name, String description, String category, BigDecimal unitPrice) {
+        this.productId = productId;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.unitPrice = unitPrice;
+    }
+    public String getProductId() {
+        return productId;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public BigDecimal getUnitPrice()
+    {
+        return unitPrice;
+    }
 }
