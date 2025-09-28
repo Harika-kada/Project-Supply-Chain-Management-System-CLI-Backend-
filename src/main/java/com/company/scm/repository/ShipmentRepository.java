14ea64f928ed4b31efa70abd8472aeac499f93ce
Harika Kada <harikakada1024@outlook.com>
2025-09-28 21:58:45 +0530

    Updated ShipmentRepository.java

--- src/main/java/com/company/scm/repository/ShipmentRepository.java
+++ src/main/java/com/company/scm/repository/ShipmentRepository.java
@@ -1,5 +1,29 @@
 package com.company.scm.repository;
 
-public class ShipmentRepository {
+import com.company.scm.model.Shipment;
+import com.company.scm.util.LoggerUtil;
 
+import java.util.*;
+
+public class ShipmentRepository {
+    private final Map<String, Shipment> store = new HashMap<>();
+
+    public void save(Shipment shipment) {
+        store.put(shipment.getShipmentId(), shipment);
+        LoggerUtil.info("Shipment saved: " + shipment.getShipmentId());
+    }
+
+    public Shipment findById(String shipmentId) {
+        return store.get(shipmentId);
+    }
+
+    public List<Shipment> findAll() {
+        LoggerUtil.info("Fetching all " + store.size() + " shipment details.");
+        return new ArrayList<>(store.values());
+    }
+
+    public void clear() {
+        store.clear();
+        LoggerUtil.debug("Shipment repository cleared.");
+    }
 }
