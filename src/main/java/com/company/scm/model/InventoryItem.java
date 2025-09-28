76ef3e2bdfd1f8fe51fc4cf46655886a0618bb5c
Harika Kada <harikakada1024@outlook.com>
2025-09-28 21:53:10 +0530

    Updated InventoryItem.java

--- src/main/java/com/company/scm/model/InventoryItem.java
+++ src/main/java/com/company/scm/model/InventoryItem.java
@@ -1,5 +1,40 @@
 package com.company.scm.model;
 
 public class InventoryItem {
+    private final String itemId;
+    private final String productId;
+    private final String warehouseId;
+    private int quantity;
+    private LocalDate lastRestockedDate;
 
+    public InventoryItem(String productId, String warehouseId, int quantity, LocalDate lastRestockedDate) {
+        this.productId = productId;
+        this.warehouseId = warehouseId;
+        this.itemId = productId + "-" + warehouseId;
+        this.quantity = quantity;
+        this.lastRestockedDate = lastRestockedDate;
+    }
+
+    public String getItemId()
+    {
+        return itemId;
+    }
+    public static String generateCompositeKey(String productId, String warehouseId) {
+        return productId + "_" + warehouseId;
+    }
+    public String getProductId() {
+        return productId;
+    }
+    public String getWarehouseId() {
+        return warehouseId;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public void setLastRestockedDate(LocalDate lastRestockedDate) {
+        this.lastRestockedDate = lastRestockedDate;
+    }
 }
