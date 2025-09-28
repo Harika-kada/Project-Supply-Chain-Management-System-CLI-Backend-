df8183b97973f58c600ac94b176071dc0dde50b8
Harika Kada <harikakada1024@outlook.com>
2025-09-28 21:58:19 +0530

    Updated SalesOrderRepository.java

--- src/main/java/com/company/scm/repository/SalesOrderRepository.java
+++ src/main/java/com/company/scm/repository/SalesOrderRepository.java
@@ -1,5 +1,41 @@
 package com.company.scm.repository;
 
-public class SalesOrderRepository {
+import com.company.scm.model.SalesOrder;
+import com.company.scm.util.LoggerUtil;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SalesOrderRepository {
+    private final Map<String, SalesOrder> store = new HashMap<>();
+
+    public void save(SalesOrder order) {
+        store.put(order.getOrderId(), order);
+        LoggerUtil.info("SalesOrder saved: " + order.getOrderId());
+    }
+
+    public SalesOrder findById(String orderId) {
+        return store.get(orderId);
+    }
+
+    public List<SalesOrder> findAll() {
+        LoggerUtil.info("Fetching all " + store.size() + " sales order details.");
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(SalesOrder order) {
+        LoggerUtil.info("Updating the sales order " + order.getOrderId());
+        store.put(order.getOrderId(), order);
+    }
+
+    public void delete(String orderId) {
+        LoggerUtil.info("Deleting the sales order " + orderId);
+        store.remove(orderId);
+    }
+    public void clear() {
+        store.clear();
+        LoggerUtil.debug("SalesOrder repository cleared.");
+    }
 }
