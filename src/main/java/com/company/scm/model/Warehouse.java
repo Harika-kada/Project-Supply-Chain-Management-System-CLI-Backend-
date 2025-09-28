a7cfe72f968fb566f5870b53cb7ff82eacf1cba7
Harika Kada <harikakada1024@outlook.com>
2025-09-28 21:55:45 +0530

    Updated Warehouse.java

--- src/main/java/com/company/scm/model/Warehouse.java
+++ src/main/java/com/company/scm/model/Warehouse.java
@@ -1,5 +1,23 @@
 package com.company.scm.model;
 
 public class Warehouse {
+    private String warehouseId;
+    private String location;
+    private int capacity;
 
+    public Warehouse(String warehouseId, String location, int capacity) {
+        this.warehouseId = warehouseId;
+        this.location = location;
+        this.capacity = capacity;
+    }
+    public String getWarehouseId() {
+        return warehouseId;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public int getCapacity()
+    {
+        return capacity;
+    }
 }
