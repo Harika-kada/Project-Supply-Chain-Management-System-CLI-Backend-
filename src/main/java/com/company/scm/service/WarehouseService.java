083221c7dac539ef98e886120c5c24642727bed9
Harika Kada <harikakada1024@outlook.com>
2025-09-28 22:01:35 +0530

    Updated WarehouseService.java

--- src/main/java/com/company/scm/service/WarehouseService.java
+++ src/main/java/com/company/scm/service/WarehouseService.java
@@ -1,5 +1,35 @@
 package com.company.scm.service;
 
-public class WarehouseService {
+import com.company.scm.model.Warehouse;
+import com.company.scm.exception.DuplicateEntityException;
+import com.company.scm.exception.EntityNotFoundException;
+import com.company.scm.repository.WarehouseRepository;
+import com.company.scm.util.CLIPrinter;
 
+import java.util.List;
+
+public class WarehouseService {
+    private final WarehouseRepository warehouseRepository;
+
+    public WarehouseService(WarehouseRepository warehouseRepository) {
+        this.warehouseRepository = warehouseRepository;
+    }
+    public void addWarehouse(Warehouse warehouse) throws DuplicateEntityException {
+        CLIPrinter.printHeader("Adding Warehouse");
+        if (warehouseRepository.findById(warehouse.getWarehouseId()) != null) {
+            throw new DuplicateEntityException("Warehouse already exists: " + warehouse.getWarehouseId());
+        }
+        warehouseRepository.addWarehouse(warehouse.getWarehouseId(), warehouse.getLocation(), warehouse.getCapacity());
+    }
+    public Warehouse getWarehouse(String warehouseId) throws EntityNotFoundException {
+        Warehouse warehouse = warehouseRepository.findById(warehouseId);
+        if (warehouse == null) {
+            throw new EntityNotFoundException(warehouseId);
+        }
+        return warehouse;
+    }
+
+    public List<Warehouse> getAllWarehouses() {
+        return warehouseRepository.findAll();
+    }
 }
