4a2f4981714b007c0bc86fc0e223ac097d05c98e
Harika Kada <harikakada1024@outlook.com>
2025-09-28 22:02:24 +0530

    Updated LoggerUtil.java

--- src/main/java/com/company/scm/util/LoggerUtil.java
+++ src/main/java/com/company/scm/util/LoggerUtil.java
@@ -1,5 +1,34 @@
 package com.company.scm.util;
 
-public class LoggerUtil {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class LoggerUtil
+{
+    private static final Logger logger = LogManager.getLogger(LoggerUtil.class);
+
+    public static Logger getLogger()
+    {
+        return logger;
+    }
+    public static void info(String message)
+    {
+        logger.info(message);
+    }
+    public static void warn(String message)
+    {
+        logger.warn(message);
+    }
+    public static void error(String message)
+    {
+        logger.error(message);
+    }
+    public static void fatal(String message)
+    {
+        logger.fatal(message);
+    }
+    public static void debug(String message)
+    {
+        logger.debug(message);
+    }
 }
