bfcdb7d2e22b0f829ae5094cac1cefb779702829
Harika Kada <harikakada1024@outlook.com>
2025-09-28 21:55:27 +0530

    Updated Supplier.java

--- src/main/java/com/company/scm/model/Supplier.java
+++ src/main/java/com/company/scm/model/Supplier.java
@@ -1,5 +1,31 @@
 package com.company.scm.model;
 
 public class Supplier {
+    private String supplierId;
+    private String name;
+    private String contactInfo;
+    private double rating;
 
+    public Supplier(String supplierId, String name, String contactInfo, double rating) {
+        this.supplierId = supplierId;
+        this.name = name;
+        this.contactInfo = contactInfo;
+        this.rating = rating;
+    }
+    public String getSupplierId()
+    {
+        return supplierId;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getContactInfo()
+    {
+        return contactInfo;
+    }
+    public double getRating()
+    {
+        return rating;
+    }
 }
