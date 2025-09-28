0d8dfcdb4d0d97343b62effaf95449b3b54462d2
Harika Kada <harikakada1024@outlook.com>
2025-09-28 22:02:01 +0530

    Updated CLIPrinter.java

--- src/main/java/com/company/scm/util/CLIPrinter.java
+++ src/main/java/com/company/scm/util/CLIPrinter.java
@@ -1,5 +1,11 @@
 package com.company.scm.util;
 
 public class CLIPrinter {
+    public static void printHeader(String title) {
+        LoggerUtil.info("--- " + title + " ---");
+    }
 
+    public static void printDivider() {
+        LoggerUtil.info("----------------------");
+    }
 }
